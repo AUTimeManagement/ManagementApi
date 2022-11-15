a865d1614ee05f9ba1f47553bd7ac19728f4da8e
DAbraham2 <daniel.abraham@edu.bme.hu>
2022-11-15 09:49:29 +0100

    Added base projects and tests

---
+++ AUTimeManagement.Api/src/Business.Logic/BusinessLogicIServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AUTimeManagement.Api.Business.Logic;
+
+public static class BusinessLogicIServiceCollectionExtensions
+{
+    public static IServiceCollection AddBusinessLogic(this IServiceCollection services)
+    {
+        return services;
+    }
+}
+
---
+++ AUTimeManagement.Api/src/Management.Api/Extensions/ApiServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using AUTimeManagement.Api.Business.Logic;
+
+namespace AUTimeManagement.Api.Management.Api.Extensions;
+
+public static class ApiServiceCollectionExtensions
+{
+    public static WebApplicationBuilder AddServices(this WebApplicationBuilder builder)
+    {
+        //TODO add honeycomb
+
+        builder.Services.ConfigureServices();
+        return builder;
+    }
+
+    private static IServiceCollection ConfigureServices(this IServiceCollection services)
+    {
+        services.AddCustomSecurity();
+
+        services.AddBusinessLogic();
+
+
+
+        services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+
+    private static void AddCustomSecurity(this IServiceCollection services)
+    {
+
+    }
+}
+
